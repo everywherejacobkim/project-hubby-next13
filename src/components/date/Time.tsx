b5e3c61f4626790bbd2dafa4fef9b119f6b647f7
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-08-11 23:07:57 -0700

    Fix: revise useEffect hook for time comp & clean up

--- src/components/date/Time.tsx
+++ src/components/date/Time.tsx
@@ -1,63 +1,57 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
 
-import { useEffect, useState } from 'react';
+const CurrentTime: React.FC = () => {
+  const [currentTime, setCurrentTime] = useState<string>(getCurrentTime());
+  const [isClient, setIsClient] = useState(false);
 
-const CurrentTime:React.FC = () => {
-
-    
-
+  function getCurrentTime(): string {
     const now = new Date();
-        
-    function getCurrentTime(): string {
-        const hours = now.getHours().toString().padStart(2, '0');
-        const minutes = now.getMinutes().toString().padStart(2, '0');
-        const seconds = now.getSeconds().toString().padStart(2, '0');
-      
-        return `${hours}:${minutes}:${seconds}`;
-      }
-    
-      const [currentTime, setCurrentTime] = useState<string>(getCurrentTime());
-      const [isClient, setIsClient] = useState(false)
- 
-      useEffect(() => {
-        setIsClient(true)
-      }, [])
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
 
-      useEffect(() => {
-        const intervalId = setInterval(() => {
-          setCurrentTime(getCurrentTime());
-        }, 1000); // Update time every 1000 milliseconds (1 second)
-    
-        return () => {
-          clearInterval(intervalId); // Cleanup the interval on component unmount
-        };
-      }, [currentTime]);// keep update time
-    
-    
-    // console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+    return `${hours}:${minutes}:${seconds}`;
+  }
 
-    console.log({currentTime})
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    // Searching time zone
-    const options: Intl.DateTimeFormatOptions = {
-        timeZoneName: 'short',
-       
-      };
-      
-    const timeZone: string = new Intl.DateTimeFormat('en-US', options).format(now)
-   
-    let splitTimeZone = timeZone.split(" ");
-    console.log(splitTimeZone);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
 
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return (
-        <div className='box-content w-full'>
-            <h1 className='font-semibold'>{splitTimeZone[1]}</h1>
-                <div className='mt-24'>
-                    {isClient ? <h1 className='sm:text-3xl text-left font-medium'> {currentTime}</h1> : ('nothing')}
-                </div> 
-        </div>
-    );
-}
+  // Searching time zone
+  const options: Intl.DateTimeFormatOptions = {
+    timeZoneName: "short",
+  };
+
+  const timeZone: string = new Intl.DateTimeFormat("en-US", options).format(
+    new Date()
+  );
+
+  let splitTimeZone = timeZone.split(" ");
+  console.log(splitTimeZone);
+
+  return (
+    <div className="box-content w-full">
+      <h1 className="font-semibold">{splitTimeZone[1]}</h1>
+      <div className="mt-24">
+        {isClient ? (
+          <h1 className="sm:text-3xl text-left font-medium"> {currentTime}</h1>
+        ) : (
+          "nothing"
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default CurrentTime;
