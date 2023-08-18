568b4e2bd87e02d0cd309de37f2a490e8a45e3a5
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-08-18 16:31:09 -0700

    Chore: add metadata head to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
-'use client';
+"use client";
 import "./globals.css";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import DashboardNav from "@/components/nav/DashboardNav";
+
+export const metadata = {
+  title: "Hubby",
+  description:
+    "Hubby is a web application designed to help desktop users in organizing their schedules, managing to-do lists, staying updated with daily news, checking emails, and utilizing an AI chatbot.",
+};
 
 export default function RootLayout({
   children,
@@ -11,7 +17,7 @@ export default function RootLayout({
   const pathname = usePathname();
   return (
     <html lang="en">
-      { pathname !== "/" && pathname !== "/dashboard" && <DashboardNav /> }
+      {pathname !== "/" && pathname !== "/dashboard" && <DashboardNav />}
       <body className="">{children}</body>
     </html>
   );
