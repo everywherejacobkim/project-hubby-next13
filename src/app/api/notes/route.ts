37a88a6f9219680d13392bf4d71801da5b1fcb9c
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-08-19 20:59:11 -0700

    Feat: create api handler for single note

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -29,47 +29,15 @@ export const POST = async (request: { json: () => Promise<any> }) => {
   }
 };
 
-// import { createNote, getAllNotes, deleteNote } from "../../../../prisma/note";
-// import { NextApiRequest, NextApiResponse } from "next";
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   try {
-//     switch (req.method) {
-//       case "POST": {
-//         const { content, author, authorEmail } = req.body;
-
-//         if (!content || !author || !authorEmail) {
-//           return res.status(400).json({ message: "Missing required fields" });
-//         }
-
-//         const new_note = await createNote(content, author, authorEmail);
-//         return res.status(201).json(new_note);
-//       }
-//       case "GET": {
-//         const notes = await getAllNotes();
-//         return res.status(200).json(notes);
-//       }
-//       case "DELETE": {
-//         const { id } = req.query;
-
-//         if (!id) {
-//           return res.status(400).json({ message: "Missing id parameter" });
-//         }
-
-//         await deleteNote(id);
-//         return res.status(200).json({
-//           message: "Note deleted successfully",
-//         });
-//       }
-//       default: {
-//         return res.status(405).json({ message: "Method not allowed" });
-//       }
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ message: "Internal server error" });
-//   }
-// }
+export const GET = async () => {
+  try {
+    const notes = await prisma.note.findMany();
+    return NextResponse.json(notes);
+  } catch (error) {
+    console.error("Error getting notes:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
